99a4efbcc6c0cd7537bc02f3baaf237f7b38bc81
pengshiquan <1162210866@qq.com>
2021-01-12 10:53:52 +0800

    增加了并发的练习代码
    增加了并发的练习代码

---
+++ src/syncTrain/ThreadPoolTest.java
@@ -0,0 +1,14 @@
+package syncTrain;
+
+/**
+ * ThreadPoolTest.java
+ * Description:  线程池的练习
+ *
+ * @author Peng Shiquan
+ * @date 2021/1/5
+ */
+public class ThreadPoolTest {
+    public static void main(String[] args) {
+        
+    }
+}
